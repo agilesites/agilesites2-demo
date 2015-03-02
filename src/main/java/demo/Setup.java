0a1f4ba13c5524218ed55cc1570a31ad092a0230
Michele Sciabarra <michele+youtrack@sciabarra.com>
2015-03-02 10:01:38 +0100

    fixed a leftover

--- src/main/java/demo/Setup.java
+++ src/main/java/demo/Setup.java
@@ -7,18 +7,8 @@ import COM.FutureTense.Interfaces.IList;
 
 public class Setup extends wcs.java.Setup {
 
-    public String exec(ICS ics, String site, String username, String password) {
-    	StringBuilder sb = new StringBuilder();
-    	sb.append("sono qui\n");
-        ElementCatalog ec = new ElementCatalog(ics, username, password);
-        ec.replace("Sono/Una/Prova.jsp", "<%=\n3+3\n%>\n" );
-        ec.close();
-
-    	return sb + super.exec(ics, site, username, password);
-	}
-
 	@Override
 	public Class<?>[] getAssets() {
-		return new Class<?>[0]; // Util.classesFromResource(Config.site, "elements.txt");
+        Util.classesFromResource(Config.site, "elements.txt");
 	}
 }
